17e380afadcbd1a70819606b0fb3a3c50aad999f
Mitalicops <120451953+Mitalicops@users.noreply.github.com>
2026-03-13 19:43:27 +0500

    fixes

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,6 @@ const nextConfig: NextConfig = {
       "images.unsplash.com",
     ],
   },
-
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
